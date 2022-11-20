57878d09f2fac373e3e4b7913fe891e192d3c629
Zobair Hussain <49007316+ZobairRUET@users.noreply.github.com>
2022-11-21 01:01:19 +0600

    Server Streaming

---
+++ gRPC_Server_Stream/server/GreetingServiceImpl.cs
@@ -0,0 +1,22 @@
+﻿using Greet;
+using Grpc.Core;
+using static Greet.GreetingService;
+
+namespace server
+{
+    internal class GreetingServiceImpl : GreetingServiceBase
+    {
+        public override async Task GreetManyTimes(GreetManyTimeRequest request, IServerStreamWriter<GreetManyTimeResponse> responseStream, ServerCallContext context)
+        {
+            Console.WriteLine($"The server received the request : ");
+            Console.WriteLine(request.ToString());
+
+            string result = String.Format($"hello {request.Greeting.FirstName} {request.Greeting.LastName}");
+
+            foreach (int i in Enumerable.Range(1, 10))
+            {
+                await responseStream.WriteAsync(new GreetManyTimeResponse() { Result = result });
+            }
+        }
+    }
+}
