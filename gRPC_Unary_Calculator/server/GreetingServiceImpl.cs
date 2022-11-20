eb4cff195ac96c52b63720e206a3e5042579cd4f
Zobair Hussain <49007316+ZobairRUET@users.noreply.github.com>
2022-11-21 00:48:47 +0600

    Calculator

---
+++ gRPC_Unary_Calculator/server/GreetingServiceImpl.cs
@@ -0,0 +1,16 @@
+﻿using Greet;
+using Grpc.Core;
+using static Greet.GreetingService;
+
+namespace server
+{
+    internal class GreetingServiceImpl : GreetingServiceBase
+    {
+        public override Task<GreetingResponse> Greet(GreetingRequest request, ServerCallContext context)
+        {
+            string result = string.Format($"hello {request.Greeting.FirstName} {request.Greeting.LastName}");
+
+            return Task.FromResult(new GreetingResponse() { Result = result });
+        }
+    }
+}
