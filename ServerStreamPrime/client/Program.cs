88126eb083ca42bba7bab32aed72e5a6d76d3bf4
Zobair Hussain <49007316+ZobairRUET@users.noreply.github.com>
2022-11-21 02:14:18 +0600

    ServerStreamPrime

---
+++ ServerStreamPrime/client/Program.cs
@@ -0,0 +1,29 @@
+﻿using Grpc.Core;
+using Prime;
+
+namespace client
+{
+    internal class Program
+    {
+        const string target = "127.0.0.1:50051";
+        static async Task Main(string[] args)
+        {
+            Channel channel = new Channel(target, ChannelCredentials.Insecure);
+
+            await channel.ConnectAsync().ContinueWith((task) =>
+            {
+                if (task.Status == TaskStatus.RanToCompletion)
+                    Console.WriteLine("The client connected successfully");
+            });
+
+            var client = new PrimeNumberService.PrimeNumberServiceClient(channel);
+            var request = new PrimeNumberDecompositionRequest() { Number = 120 };
+            var response = client.PrimeNumberDecomposion(request);
+            while (await response.ResponseStream.MoveNext())
+            {
+                Console.WriteLine(response.ResponseStream.Current.PrimeFactor);
+                await Task.Delay(2000);
+            }
+        }
+    }
+}
---
+++ ServerStreamPrime/server/Program.cs
@@ -0,0 +1,36 @@
+﻿using Grpc.Core;
+using Prime;
+
+namespace server
+{
+    internal class Program
+    {
+        const int Port = 50051;
+        static void Main(string[] args)
+        {
+            Server? server = null;
+            try
+            {
+                server = new Server()
+                {
+                    Services = {PrimeNumberService.BindService( new PrimeServiceImpl())},
+                    Ports = { new ServerPort("localhost", Port, ServerCredentials.Insecure) }
+                };
+                server.Start();
+                Console.WriteLine($"The server is listening on the port : {Port}");
+                Console.ReadKey();
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine($"The server failed to start : {e.Message}");
+            }
+            finally
+            {
+                if (server != null)
+                {
+                    server.ShutdownAsync().Wait();
+                }
+            }
+        }
+    }
+}
